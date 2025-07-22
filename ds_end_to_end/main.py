967208c283555da342bae5f93c3a2d4ddd4a47c6
Mushfiqur-Rahman-Robin <mushfiqur.rahman.robin@gmail.com>
2025-07-22 15:41:32 +0600

    added data validation pipeline

--- ds_end_to_end/main.py
+++ ds_end_to_end/main.py
@@ -1,5 +1,6 @@
 from src.ds_end_to_end import logger
 from src.ds_end_to_end.pipeline.data_ingestion_pipe import DataIngestionTrainingPipeline
+from src.ds_end_to_end.pipeline.data_validation_pipe import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -8,6 +9,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.initialize_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
--- ds_end_to_end/src/ds_end_to_end/config/configuration.py
+++ ds_end_to_end/src/ds_end_to_end/config/configuration.py
@@ -2,7 +2,7 @@ import sys
 from src.ds_end_to_end.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH, SCHEMA_FILE_PATH
 from src.ds_end_to_end.utils.common import read_yaml, create_directories
 from pathlib import Path
-from src.ds_end_to_end.entity.config_entity import (DataIngestionConfig)
+from src.ds_end_to_end.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -40,3 +40,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema
+        )
+
+        return data_validation_config

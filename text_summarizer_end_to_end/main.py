02a4c5508a3997c40883291ac8536dbd586df60e
Mushfiqur-Rahman-Robin <u1604123@student.cuet.ac.bd>
2025-07-27 20:36:35 +0600

    model trainer implementation

--- text_summarizer_end_to_end/main.py
+++ text_summarizer_end_to_end/main.py
@@ -1,6 +1,7 @@
 from src.textsummarizer.logging import logger
 from src.textsummarizer.pipeline.data_ingestion_pipe import DataIngestionTrainingPipeline
 from src.textsummarizer.pipeline.data_transformation_pipe import DataTransformationTrainingPipeline
+from src.textsummarizer.pipeline.model_trainer_pipe import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -20,6 +21,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataTransformationTrainingPipeline()
     obj.initiate_data_transformation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.initiate_model_training()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)

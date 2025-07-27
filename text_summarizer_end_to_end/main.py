ff19567d882ef935dc9fd983e964efacf3911c58
Mushfiqur-Rahman-Robin <u1604123@student.cuet.ac.bd>
2025-07-27 18:57:36 +0600

    data transformation steps added

--- text_summarizer_end_to_end/main.py
+++ text_summarizer_end_to_end/main.py
@@ -1,5 +1,7 @@
 from src.textsummarizer.logging import logger
 from src.textsummarizer.pipeline.data_ingestion_pipe import DataIngestionTrainingPipeline
+from src.textsummarizer.pipeline.data_transformation_pipe import DataTransformationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -7,6 +9,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.initiate_data_transformation()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
--- text_summarizer_end_to_end/src/textsummarizer/config/configuration.py
+++ text_summarizer_end_to_end/src/textsummarizer/config/configuration.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from src.textsummarizer.constants import *
 from src.textsummarizer.utils.common import read_yaml, create_directories
 from src.textsummarizer.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
-from src.textsummarizer.entity.config_entity import DataIngestionConfig
+from src.textsummarizer.entity.config_entity import DataIngestionConfig, DataTransformationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -30,3 +30,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name=config.tokenizer_name
+        )
+
+        return data_transformation_config

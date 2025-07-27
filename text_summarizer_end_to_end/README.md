4186257372f7077650f58339cba6f897c8f66213
Mushfiqur-Rahman-Robin <u1604123@student.cuet.ac.bd>
2025-07-27 18:08:29 +0600

    end to end text summarization project (ongoing)

---
+++ text_summarizer_end_to_end/README.md
@@ -0,0 +1,11 @@
+# textsummarizer using huggingface
+
+# workflows
+
+1. config.yaml
+2. params.yaml
+3. config entity
+4. configuration manager
+5. components - data ingestion, data transformation, data trainer
+6. pipeline - training pipeline, prediction pipeline
+7. frontend apis, batch prediction apis, train apis
---
+++ text_summarizer_end_to_end/main.py
@@ -0,0 +1,13 @@
+from src.textsummarizer.logging import logger
+from src.textsummarizer.pipeline.data_ingestion_pipe import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
